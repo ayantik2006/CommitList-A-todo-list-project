b1704551114485acb2d5ecf26930f351852343fc
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-23 21:05:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-const buildPath = path.join(__dirname, "frontend", "dist");
+const buildPath = path.join("../", "frontend", "dist");
 app.use(express.static(buildPath));
 
 app.set("view engine","ejs");
