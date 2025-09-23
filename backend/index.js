376cb0e89feeea157eaff864f5a82d0823f0f2da
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-23 21:52:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,6 @@ const todoRoutes=require("./routes/todo.js");
 const { mongo } = require("mongoose");
 app.use("/auth",authRoutes);
 app.use("/todo",todoRoutes);
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(buildPath, "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
 });
