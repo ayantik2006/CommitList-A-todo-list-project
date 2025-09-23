8f26c7e1691af81cca887bcdb86b3b88b811c449
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-23 14:13:52 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,13 @@ const MONGO_URL=process.env.MONGO_URL;
 
 const mongodb = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
   } 
   catch (err) {}
 };
 
 module.exports = mongodb;
+
