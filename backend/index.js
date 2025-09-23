6d418658366e8241e7064c4a704a99fa7edb2fc0
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-23 12:46:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://commitlist-a-todo-list-project.onrender.com", 
   methods: ["GET", "POST", "PUT", "DELETE"], 
   credentials: true 
 }));
