b2fd49b6cba563d9970980075aee8e2d7c9e889f
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-24 00:00:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use("/todo", todoRoutes);
 
 // Catch-all route for React Router
 // Catch-all route for React
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
